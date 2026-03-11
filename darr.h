e94e21b535f91b7f7353b346f92d03154c95e301
MrNimbus777 <114327199+MrNimbus777@users.noreply.github.com>
2026-03-11 15:19:09 +0200

    Add files via upload

---
+++ darr.h
@@ -0,0 +1,69 @@
+#ifndef DARR_H
+#define DARR_H
+
+#include <stdlib.h>
+
+
+// TODO: - reserve, shrink_to_fit, clear, insert
+
+// TYPE: for pointers provide a wrapping type (e.g. char* -> typedef char* my_string)
+#define DARR_IMPLEMENT_EXPLICIT(TYPE, ARR_STRUCT_NAME)                                            \
+                                                                                                  \
+typedef struct {                                                                                  \
+    TYPE* elements;                                                                               \
+    size_t size;                                                                                  \
+    size_t capacity;                                                                              \
+} ARR_STRUCT_NAME;                                                                                \
+                                                                                                  \
+static void ARR_STRUCT_NAME##_init(ARR_STRUCT_NAME* arr) {                                        \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    arr->elements = NULL;                                                                         \
+    arr->size = 0;                                                                                \
+    arr->capacity = 0;                                                                            \
+}                                                                                                 \
+static void ARR_STRUCT_NAME##_init_with_capacity(ARR_STRUCT_NAME* arr, size_t initial_capacity) { \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    arr->elements = (TYPE*)malloc(initial_capacity * sizeof(TYPE));                               \
+    arr->size = 0;                                                                                \
+    arr->capacity = initial_capacity;                                                             \
+}                                                                                                 \
+                                                                                                  \
+static void ARR_STRUCT_NAME##_free(ARR_STRUCT_NAME* arr) {                                        \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    if(arr->elements) free(arr->elements);                                                        \
+    arr->elements = NULL;                                                                         \
+    arr->size = 0;                                                                                \
+    arr->capacity = 0;                                                                            \
+}                                                                                                 \
+                                                                                                  \
+static void ARR_STRUCT_NAME##_push(ARR_STRUCT_NAME* arr, TYPE value) {                            \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    if (arr->size >= arr->capacity) {                                                             \
+        size_t new_capacity = arr->capacity == 0 ? 4 : arr->capacity * 2;                         \
+        TYPE* new_data = (TYPE*)realloc(arr->elements,                                            \
+                                new_capacity * sizeof(TYPE));                                     \
+        if (!new_data) exit(EXIT_FAILURE);                                                        \
+        arr->elements = new_data;                                                                 \
+        arr->capacity = new_capacity;                                                             \
+    }                                                                                             \
+    arr->elements[arr->size++] = value;                                                           \
+}                                                                                                 \
+                                                                                                  \
+static TYPE ARR_STRUCT_NAME##_pop(ARR_STRUCT_NAME* arr) {                                         \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    assert(arr->size >= 0 && "An array with positive size is expected")                                            \
+    return arr->elements[--arr->size];                                                            \
+}                                                                                                 \
+                                                                                                  \
+static void ARR_STRUCT_NAME##_remove(ARR_STRUCT_NAME* arr, size_t index){                         \
+    assert(arr != NULL && "A valid array is expected")                                            \
+    assert(index < arr->size && "Index out of range")                                            \
+    arr->size--;                                                                                  \
+    for(size_t i = index; i < arr->size; i++) {                                                   \
+        arr->elements[i] = arr->elements[i+1];                                                    \
+    }                                                                                             \
+}
+
+#define DARR_IMPLEMENT(TYPE) DARR_IMPLEMENT_EXPLICIT(TYPE, TYPE##_darr)
+
+#endif
